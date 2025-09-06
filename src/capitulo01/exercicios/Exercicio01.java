437524647bea20deee41a287f7493be6321f4027
Carlos <carlosiilva66@gmail.com>
2025-09-06 11:34:51 -0300

    init: primeira versão do projeto com configuração inicial

---
+++ src/capitulo01/exercicios/Exercicio01.java
@@ -0,0 +1,16 @@
+package capitulo01.exercicios;
+
+/**
+ *
+ */
+public class Exercicio01 {
+
+    /**
+     *
+     * @param args
+     */
+    public static public static void main(String[] args) {
+
+        
+    }
+}
